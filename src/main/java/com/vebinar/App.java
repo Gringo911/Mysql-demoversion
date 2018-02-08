83800f082cca01d77d92c2181219b15b7a9ef7f7
vasy911 <irbisaltai@yandex.ua>
2018-02-08 23:39:09 +0300

    ogo

--- src/main/java/com/vebinar/App.java
+++ src/main/java/com/vebinar/App.java
@@ -1,21 +1,9 @@
 package com.vebinar;
 
-import config.SpringConfig;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import service.TestBean;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) {
-
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-        TestBean bean = context.getBean(TestBean.class);
-        String name = bean.getName();
-
-        System.out.println( "Hello World!" + name );
-    }
 }
