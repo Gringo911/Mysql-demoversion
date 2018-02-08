13fd9414f25de5922b85eef790987143a456fce3
vasy911 <irbisaltai@yandex.ua>
2018-02-09 00:23:46 +0300

    ru

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,6 +1,7 @@
 package config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
  */
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = "controller")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,7 +1,17 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by ВАСЯ on 06.02.2018.
  */
+@Controller
+@RequestMapping("/")
 public class UserController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
